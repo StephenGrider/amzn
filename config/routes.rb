9c77792a3f183c0f0fac42d02b13e84237ebe744
Stephen Grider <ste.grider@gmail.com>
2015-01-04 17:18:33 -0800

    line items controller + views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
-  resources :items
+  resources :items, :only => [:index, :show]
+  resources :line_items, :only => [:index]
 end
