6b6afe07af1895eeb158bbf7e2e1ff815f50c851
Stephen Grider <ste.grider@gmail.com>
2015-01-05 19:00:13 -0800

    line_items can be deleted

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
   has_many :line_items
   has_many :users, :through => :line_items
+  
+  def is_liked?(user)
+    user.items.include?(self)
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,12 @@ class LineItem < ActiveRecord::Base
   belongs_to :item
   
   validates :user_id, :uniqueness => { :scope => :item_id }
+  
+  def can_destroy?(user)
+    unless user.id == self.user.id
+      errors.add(:base, "You must own a line_item to destroy it")
+      return false
+    end
+    return true
+  end
 end
