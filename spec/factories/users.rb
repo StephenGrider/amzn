b65b528149e041c4d1cddcebb3f5e8fe0f461968
StephenGrider <ste.grider@gmail.com>
2015-01-06 19:01:40 -0800

    item and line_item specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 end
