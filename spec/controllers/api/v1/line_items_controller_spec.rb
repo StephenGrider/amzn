ad05910213ba59cf0d72f232d8b38b693a6ce0ce
Stephen Grider <ste.grider@gmail.com>
2015-01-08 18:54:33 -0800

    specs for line items controller

--- spec/controllers/api/v1/line_items_controller_spec.rb
+++ spec/controllers/api/v1/line_items_controller_spec.rb
@@ -2,32 +2,69 @@ require 'spec_helper'
 
 describe Api::V1::LineItemsController, :type => :controller do 
   
-  describe '#index' do
-    
+  before do
+    @user = create(:user)
+  end
+  
+  describe '#index' do    
+    it 'returns all line_items' do
+      FactoryGirl.create_list(:line_item, 5)
+      
+      get :index
+      
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response.length).to eq(5)
+      expect(parsed_response.first['user_id']).to_not be_nil
+      expect(parsed_response.first['item_id']).to_not be_nil
+    end
   end
   
   describe '#create' do
+    before do
+      sign_in @user
+    end
+    
     context 'valid attributes' do
+      attrs = nil
+      
+      before do
+        attrs = FactoryGirl.attributes_for(:line_item)
+          .merge(user_id: @user.id, item_id: create(:item).id)
+      end
+      
       it 'creates a new line_item' do
         expect{
-          post :create, line_item: FactoryGirl.attributes_for(:line_item)
+          post :create, line_item: attrs
         }.to change(LineItem, :count).by(1)
       end
       
       it 'returns the new model' do
-        expect(response).to be_blank
+        post :create, line_item: attrs
+
+        parsed_response = JSON.parse(response.body)
+        expect(parsed_response["line_item"]["user_id"]).to eq(attrs[:user_id])
+        expect(parsed_response["line_item"]["item_id"]).to eq(attrs[:item_id])
       end
     end
       
-      context 'invalid attributes' do
-        it 'does not create a new line_item' do
-          
-        end
-        
-        it 'renders nothing' do
-          
-        end
+    context 'invalid attributes' do
+      attrs = nil
+      
+      before do 
+        attrs = FactoryGirl.attributes_for(:line_item)
+        attrs[:user_id] = nil
       end
+      
+      it 'does not create a new line_item' do
+        expect{
+          post :create, line_item: attrs
+        }.to change(LineItem, :count).by(0)
+      end
+      
+      it 'renders nothing' do
+        
+      end
+    end
   end
   
   describe '#destroy' do
