07857c52032922eb581dc7b69b6c5c5d89f3e0f7
StephenGrider <ste.grider@gmail.com>
2015-01-12 09:04:24 -0800

    item fetcher service object

---
+++ app/services/item_fetcher.rb
@@ -0,0 +1,15 @@
+module Services
+  class ItemFetcher
+    def self.create_items(options)
+      res = Amazon::Ecs.item_search(options[:search_string], 
+        { response_group: 'Medium', sort: 'salesrank'})
+        
+      res.items.each do |item|
+        Item.create(
+          title: item.get_element('ItemAttributes').get('Title'),
+          price: item.get_hash('MediumImage')[:url]
+        )
+      end
+    end
+  end
+end
