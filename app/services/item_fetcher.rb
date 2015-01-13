24ca946062c942279658e8538a90807fdeb9bffc
Stephen Grider <ste.grider@gmail.com>
2015-01-13 08:47:00 -0800

    GEMS

--- app/services/item_fetcher.rb
+++ app/services/item_fetcher.rb
@@ -4,11 +4,13 @@ module Services
       res = Amazon::Ecs.item_search(options[:search_string], 
         { response_group: 'Medium', sort: 'salesrank'})
         
-      res.items.each do |item|
-        Item.create(
-          title: item.get_element('ItemAttributes').get('Title'),
-          price: item.get_hash('MediumImage')[:url]
-        )
+      if res.valid_request?
+        res.items.each do |item|
+          Item.create(
+            title: item.get_element('ItemAttributes').get('Title'),
+            price: item.get_hash('MediumImage')[:url]
+          )
+        end
       end
     end
   end
