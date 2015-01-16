6c0ae8d6dbcdda9e4c47c9e533c438a4c003ccf3
Stephen Grider <ste.grider@gmail.com>
2015-01-15 22:56:50 -0800

    added api scratchpad

---
+++ app/controllers/admin/amazon_items_controller.rb
@@ -0,0 +1,14 @@
+module Admin
+  class AmazonItemsController < ApplicationController
+    def index
+      
+    end
+    
+    def search
+      res = Amazon::Ecs.item_search(params[:search_string], 
+        { response_group: 'Offers,ItemAttributes,Images', search_index: 'Shoes'})
+      
+      render json: Hash.from_xml(res.doc.to_s)
+    end
+  end
+end
