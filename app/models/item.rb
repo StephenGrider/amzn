8e788f763a5340abfb6e7c56c6759c02c3268d76
StephenGrider <ste.grider@gmail.com>
2015-01-17 14:57:59 -0800

    item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,14 @@ class Item < ActiveRecord::Base
   has_many :line_items
   has_many :users, :through => :line_items
   
+  validates :title, presence: true
+  validates :image_url, presence: true
+  validates :url, presence: true
+  validates :brand, presence: true
+  validates :price, presence: true
+  validates :wishlist_url, presence: true
+  validates :tell_friend_url, presence: true
+  
   def is_liked?(user)
     user.items.include?(self)
   end
