2e869047ba51e3839e725058cdb17d4489529814
Stephen Grider <ste.grider@gmail.com>
2015-01-23 08:50:40 -0800

    kors handling

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,13 @@ module Amzn
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
