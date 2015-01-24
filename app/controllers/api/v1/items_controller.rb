112b37fba63f3590d1b8a6caac8f7746fd5b0b96
StephenGrider <ste.grider@gmail.com>
2015-01-24 15:17:32 -0800

    refactored items_controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,15 +2,25 @@ module Api
   module V1
     class ItemsController < Api::V1::ApiController
       def index
-        json = Item.paginate(page: params[:page]).to_json
-        
-        if params[:filters][:liked]
-          json = Item.liked_by(current_user).paginate(page: params[:page]).to_json
-        elsif params[:filters][:unseen]
-          json = Item.unrated_by(current_user).paginate().to_json
+        json = Item.paginate(page: item_params[:page] || 1).to_json
+        if item_params[:liked]
+          json = Item.liked_by(current_user).paginate(page: params[:page] || 1).to_json
+        elsif item_params[:unrated]
+          json = Item.unrated_by(current_user).paginate(page: params[:page]).to_json
         end
-        
+
         render json: json
+      end
+      
+      private
+      
+      def item_params
+        {
+          page: params[:page] || 1,
+          liked: params[:liked] == "true" || false,
+          unliked: params[:unliked] == "true" || false,
+          unrated: params[:unrated] == "true" || false
+        }
       end
     end
   end
