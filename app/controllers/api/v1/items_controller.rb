80f9fe7de19c5da4528feb23d73a8caaac957b6e
Stephen Grider <ste.grider@gmail.com>
2015-01-24 13:21:52 -0800

    items index shows all, unseen, or liked items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,15 @@ module Api
   module V1
     class ItemsController < Api::V1::ApiController
       def index
-        render json: Item.all.to_json
+        json = Item.paginate(page: params[:page]).to_json
+        
+        if params[:filters][:liked]
+          json = Item.liked_by(current_user).paginate(page: params[:page]).to_json
+        elsif params[:filters][:unseen]
+          json = Item.unrated_by(current_user).paginate().to_json
+        end
+        
+        render json: json
       end
     end
   end
