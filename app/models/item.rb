8417b4927b9d8e56bbbfbd5dc8a54f410e2b4f5e
StephenGrider <ste.grider@gmail.com>
2015-01-24 13:19:58 -0800

    item scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
+  self.per_page = 20
+  
   has_many :line_items
   has_many :users, :through => :line_items
   belongs_to :search_node
@@ -10,6 +12,16 @@ class Item < ActiveRecord::Base
   validates :price, presence: true
   validates :wishlist_url, presence: true
   validates :tell_friend_url, presence: true
+  
+  scope :unseen, lambda { |user|
+    joins("left outer join line_items as i on items.id = i.item_id")
+    .where("i.user_id is not ?", user.id)
+  }
+  
+  scope :liked_by, lambda { |user|
+    joins("left outer join line_items as i on items.id = i.item_id")
+    .where("i.user_id is ?", user.id)
+  }
   
   def is_liked?(user)
     user.items.include?(self)
