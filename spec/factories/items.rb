9c69778debaaa8962c4b3c12eac8728c6e94dcde
Stephen Grider <ste.grider@gmail.com>
2015-01-24 14:09:22 -0800

    updated line_item spec

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     price { Faker::Number.number(2) }
     image_url { Faker::Internet.url }
     url { Faker::Internet.url }
+    title { Faker::Lorem.sentence }
+    brand { Faker::Lorem.sentence }
+    wishlist_url { Faker::Internet.url }
+    tell_friend_url { Faker::Internet.url }
   end
 end
