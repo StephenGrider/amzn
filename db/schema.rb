e897ce5d3777e796f9ce1bee060ea59594739485
Stephen Grider <ste.grider@gmail.com>
2015-01-23 18:34:31 -0800

    add imei to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118015512) do
+ActiveRecord::Schema.define(version: 20150124011059) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20150118015512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "imei"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
