70b3df5b492da9e9ce164932d92eff2274bb9faa
StephenGrider <ste.grider@gmail.com>
2015-01-27 18:53:29 -0800

    index can return items a user has not seen yet

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,12 +2,13 @@ module Api
   module V1
     class ItemsController < Api::V1::ApiController
       def index
-        json = Item.paginate(page: item_params[:page] || 1).to_json
         if item_params[:liked]
-          json = Item.liked_by(current_user).paginate(page: params[:page] || 1).to_json
+          json = Item.liked_by(current_user).paginate(page: item_params[:page] || 1).as_json
         elsif item_params[:unrated]
-          json = Item.unrated_by(current_user).paginate(page: params[:page]).to_json
-        end
+          json = Item.unrated_by(current_user).paginate(page: item_params[:page]).as_json
+        else
+          json = Item.paginate(page: item_params[:page] || 1).as_json
+        end 
 
         render json: json
       end
