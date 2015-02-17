f6ade3b66a9fd348034de3d37dd39b4434e8d569
Stephen Grider <ste.grider@gmail.com>
2015-02-16 20:22:16 -0800

    restructured seeds to accommodate new search node style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,119 @@
+#############
+# Amazon
+#############
+
 SearchNode.create(
   enabled: true,
-  amazon_id: 679341011,
+  vendor_id: 679341011,
   category: "Basketball",
-  parent_category: "Athletic"
+  parent_category: "Athletic",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 679360011,
+  vendor_id: 679360011,
   category: "Running",
-  parent_category: "Athletic"
+  parent_category: "Athletic",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 679377011,
+  vendor_id: 679377011,
   category: "Walking",
-  parent_category: "Athletic"
+  parent_category: "Athletic",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 679369011,
+  vendor_id: 679369011,
   category: "Skateboard",
-  parent_category: "Athletic"
+  parent_category: "Athletic",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 679374011,
+  vendor_id: 679374011,
   category: "Tennis",
-  parent_category: "Athletic"
+  parent_category: "Athletic",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 684423011,
+  vendor_id: 684423011,
   category: "Dress & Evening Flats",
-  parent_category: "Flats"
+  parent_category: "Flats",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 679401011,
+  vendor_id: 679401011,
   category: "Mary-Jane Flats",
-  parent_category: "Flats"
+  parent_category: "Flats",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 679403011,
+  vendor_id: 679403011,
   category: "Slingback Flats",
-  parent_category: "Flats"
+  parent_category: "Flats",
+  vendor: "Amazon"
 )
 
 SearchNode.create(
   enabled: true,
-  amazon_id: 683100011,
+  vendor_id: 683100011,
   category: "T-Strap Flats",
-  parent_category: "Flats"
+  parent_category: "Flats",
+  vendor: "Amazon"
+)
+
+#############
+# Rakuten
+#############
+
+SearchNode.create(
+  enabled: true,
+  vendor_id: 25783,
+  category: "Athletic Shoes",
+  parent_category: "",
+  vendor: "Rakuten"
+)
+
+SearchNode.create(
+    vendor_id: 25869,
+    category: "Boots",
+    enabled: true,
+    vendor: "Rakuten",
+    parent_category: ""
+)
+
+SearchNode.create(
+    vendor_id: 25632,
+    category: "Casual Shoes",
+    enabled: true,
+    vendor: "Rakuten",
+    parent_category: ""
+)
+
+SearchNode.create(
+    vendor_id: 25703,
+    category: "Sandals",
+    enabled: true,
+    vendor: "Rakuten",
+    parent_category: ""
+)
+
+SearchNode.create(
+    vendor_id: 25743,
+    category: "Slippers & Flip-Flops",
+    enabled: true,
+    vendor: "Rakuten",
+    parent_category: ""
 )
