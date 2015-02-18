f1dc867c4f872348bb58609ebeef3cd757dda61c
Stephen Grider <ste.grider@gmail.com>
2015-02-18 09:11:44 -0800

    popshops item fetcher fleshed out

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,42 +1,39 @@
 class Item < ActiveRecord::Base
   self.per_page = 20
-  
+
   has_many :line_items
   has_many :users, :through => :line_items
   belongs_to :search_node
-  
+
   validates :title, presence: true
   validates :image_url, presence: true
   validates :url, presence: true
-  validates :brand, presence: true
   validates :price, presence: true
-  validates :wishlist_url, presence: true
-  validates :tell_friend_url, presence: true
 
   scope :unrated_by, lambda { |user|
     where("not exists (select id from line_items where line_items.item_id = items.id AND line_items.user_id is ?)", user.id)
   }
-  
+
   scope :liked_by, lambda { |user|
     joins("left outer join line_items as i on items.id = i.item_id")
     .where("i.liked is ?", true)
     .where("i.user_id is ?", user.id)
   }
-  
+
   scope :disliked_by, lambda { |user|
     joins("left outer join line_items as i on items.id = i.item_id")
     .where("i.liked is false")
     .where("i.user_id is ?", user.id)
   }
-  
+
   def is_liked?(user)
     user.items.include?(self)
   end
-  
+
   def category
     search_node.category
   end
-  
+
   def parent_category
     search_node.parent_category
   end
--- app/services/popshops_item_fetcher.rb
+++ app/services/popshops_item_fetcher.rb
@@ -8,7 +8,8 @@ module Services
             catalog: "e5o3n125egw96por86dazlaey",
             results_per_page: "100",
             category: options[:category],
-            page: options[:page]
+            page: options[:page],
+            per_page: 1
           }
         }
       end
@@ -16,22 +17,53 @@ module Services
       def do_request
         res = HTTParty.get("http://popshops.com/v3/products.json", @req_opts)
         parse_response res
-        res
+        "done"
       end
 
       private
 
       def parse_response(res)
         return if res.code != 200
-        json = JSON.parse(res.body)
+        json = items(res)
 
-        json["results"]["products"]["product"].each do |response_item|
+        json.each do |response_item|
           item = Item.find_or_create_by(asin: get_asin(response_item))
+          item.title = get_title(response_item)
+          item.price = get_price(response_item)
+          item.image_url = get_image_url(response_item)
+          item.url = get_url(response_item)
+          # item.brand = get_brand(response_item) TODO: fix brand/brand_id
+
+          item.save!
         end
+      end
+
+      def items(res)
+        JSON.parse(res.body)["results"]["products"]["product"]
+      end
+
+      def get_title(item)
+        item["name"]
+      end
+
+      def get_brand_id(item)
+
       end
 
       def get_asin(item)
         item["id"]
+      end
+
+      def get_url(item)
+        item["offers"]["offer"].first["url"]
+      end
+
+      def get_price(item)
+        item["offers"]["offer"].first["price_merchant"]
+      end
+
+      def get_image_url(item)
+        item["image_url_large"]
       end
     end
   end
