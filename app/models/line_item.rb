35b76935a33acdb7ce3e91255492560301a8df9c
Stephen Grider <ste.grider@gmail.com>
2015-03-27 19:02:29 -0700

    LineItem validates inclusion of liked instead of validating presence

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,10 +1,11 @@
 class LineItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :item
-  
-  validates_presence_of :user_id, :item_id, :liked
+
+  validates_presence_of :user_id, :item_id
+  validates_inclusion_of :liked, in: [true, false]
   validates :user_id, uniqueness: { :scope => :item_id }
-  
+
   def can_destroy?(user)
     unless user.id == self.user.id
       errors.add(:base, "You must own a line_item to destroy it")
